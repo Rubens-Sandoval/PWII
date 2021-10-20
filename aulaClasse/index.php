468a72d03b43875c3947618d54559317e1dc2be7
Rubens Henrique Soares Sandoval <66434761+Rubens-Sandoval@users.noreply.github.com>
2021-10-20 14:50:47 -0300

    Aula de classes php
    Arquivos com os exemplos de utilização e criação de classes

---
+++ aulaClasse/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Exemplo de formulário</title>
+</head>
+<body>
+	<form method="POST" action="teste.class.php">
+		Nome: <input type="text" name="nome" id="nome"><br><br>
+		<input type="submit" name="cadastrar" value="Cadastrar">
+	</form>
+</body>
+</html>
