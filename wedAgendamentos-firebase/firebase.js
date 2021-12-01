ed2968f4f1273c448980c3f4f0ee0b84e3b8f4fe
Rubens Henrique Soares Sandoval <66434761+Rubens-Sandoval@users.noreply.github.com>
2021-12-01 16:39:20 -0300

    Web Agendamentos com Firebase
    Implementação do firebase na aplicação web-agendamentos

---
+++ wedAgendamentos-firebase/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDtTdDxxdMS8Yo-hZxgk8hHWkBrOj0Ctko",
+    authDomain: "pwii-9b7d6.firebaseapp.com",
+    projectId: "pwii-9b7d6",
+    storageBucket: "pwii-9b7d6.appspot.com",
+    messagingSenderId: "731762528577",
+    appId: "1:731762528577:web:81f634c5dee979e44891c0",
+    measurementId: "G-L6WC5LW1FQ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
